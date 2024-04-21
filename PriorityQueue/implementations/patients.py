f86238cdebc96c8b642493c7d7b020948fe0c6f3
DilanUrrego <141243258+DilanUrrego@users.noreply.github.com>
2024-04-20 19:29:55 -0500

    Update patients.py

--- PriorityQueue/implementations/patients.py
+++ PriorityQueue/implementations/patients.py
@@ -1,10 +1,61 @@
 import classes.patient as patient
 
-
 patient1 = patient.Patient(
-    "Dailan Borrego", 
+    "Jose Perez", 
     23, 
-    "This patient had a car crash, and hurt both legs, breaking several bones.",
+    "Had a car crash, and hurt both legs, breaking several bones.",
     2)
 
-# Dailan will do 9 more instances
+patient2 = patient.Patient(
+    "Leo Messi", 
+    36, 
+    "Received a strong kick while paying football.",
+    2)
+
+patient3 = patient.Patient(
+    "Evander Holyfield", 
+    23, 
+    'Fighter called "Myke Tyson" bit his right ear.',
+    2)
+
+patient4 = patient.Patient(
+    "Leonardo DiCaprio", 
+    49, 
+    "Injured on set during filming, receiving treatment for a sprained ankle.",
+    3)
+
+patient5 = patient.Patient(
+    "Dwayne Johnson", 
+    51, 
+    "Lost to papaer in a paper rock scissors game.",
+    5)
+
+patient6 = patient.Patient(
+    "Maria Garcia", 
+    45, 
+    "Suffering from severe chest pain and shortness of breath.",
+    1)
+
+patient7 = patient.Patient(
+    "Pedro Alvarez", 
+    30, 
+    "Fainted due to dehydration and exhaustion.",
+    3)
+
+patient8 = patient.Patient(
+    "Mohamed Soto", 
+    50, 
+    "Severe headache and blurred vision.",
+    4)
+
+patient9 = patient.Patient(
+    "Darwin Henao", 
+    28, 
+    "Severe allergic reaction, difficulty breathing.",
+    1)
+
+patient10 = patient.Patient(
+    "Arnulfo", 
+    55, 
+    "Suffering from a heart attack, intense chest pain.",
+    1)
